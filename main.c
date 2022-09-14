6ef1b4aeecc96a9214ee8c8a58a050040192c12a
Roope Vuorenlehto <80318201+jungleistx@users.noreply.github.com>
2022-09-14 23:56:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,9 @@ int main(void)
 	long b = 109222114;
 	char c = 'A';
 	char *str = "Hello";
-
+	int *ptr = &a;
+	float f = 42.15;
+	
 	ft_printf("int: %d\n", a);
 	ft_printf("long: %ld\n", b);
 	ft_printf("char: %c\n", c);
@@ -14,6 +16,9 @@ int main(void)
 	ft_printf("hex of value %d: %x\n", a, a);
 	ft_printf("octal of value %d: %o\n", a, a);
 	ft_printf("binary of value %d: %b\n", a, a);
-  
+	ft_printf("address of integer a: %p\n", &a);
+	ft_printf("address of integer a: %p\n", ptr);
+	ft_printf("float f: %f\n", f);
+  	
   return (0);
 }
